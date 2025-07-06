32762dca1bf47eef5dc3d42b5fe1cb04c163467c
Vidal <jmvidal@stefanini.com>
2025-07-06 20:36:20 -0300

    Tela de detalhes da nota fiscal

--- src/app/layout/nota-detalhe/nota-detalhe.component.ts
+++ src/app/layout/nota-detalhe/nota-detalhe.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NotaFiscalService, NotaFiscal } from '../../services/nota-fiscal.service';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nota-detalhe',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatTableModule
+  ],
+  providers: [CurrencyPipe, DatePipe],
   templateUrl: './nota-detalhe.component.html',
   styleUrl: './nota-detalhe.component.css'
 })
 export class NotaDetalheComponent implements OnInit {
   nota!: NotaFiscal;
+  colunasItens = ['descricao', 'quantidade', 'valorUnitario', 'valorTotal'];
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private notaService: NotaFiscalService
   ) {}
 
@@ -26,4 +39,7 @@ export class NotaDetalheComponent implements OnInit {
     });
   }
 
+  voltar(): void {
+    this.router.navigate(['/']);
+  }
 }
